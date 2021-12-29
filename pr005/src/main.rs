ab9c33dc1cbd4f6e61c48659edf5836f09e8dc2e
Mark Schultz <mdschultz@eng.ucsd.edu>
2021-12-28 21:21:46 -0800

    Fix pr005

--- pr005/src/main.rs
+++ pr005/src/main.rs
@@ -5,15 +5,20 @@ fn main() {}
 pub fn longest_palindrome(s: String) -> String {
     let s = s.as_bytes();
     // Centered at single char
-    let mut max_j_single = 0;
-    let mut max_c_single = 0;
+    let mut max_j_single = None;
+    let mut max_c_single = None;
     for c in 0..s.len() {
         let mut j = 0;
         while c >= j && c + j < s.len() {
             if s[c - j] == s[c + j] {
-                if j >= max_j_single {
-                    max_j_single = j;
-                    max_c_single = c;
+                if let Some(max_j) = max_j_single {
+                    if j >= max_j {
+                        max_j_single = Some(j);
+                        max_c_single = Some(c);
+                    }
+                } else {
+                    max_j_single = Some(j);
+                    max_c_single = Some(c);
                 }
             } else {
                 break;
@@ -22,15 +27,20 @@ pub fn longest_palindrome(s: String) -> String {
         }
     }
     // Centered at two char
-    let mut max_j_double = 0;
-    let mut max_c_double = 0;
+    let mut max_j_double = None;
+    let mut max_c_double = None;
     for c in 0..(s.len() - 1) {
         let mut j = 0;
         while c >= j && c + j + 1 < s.len() {
             if s[c - j] == s[c + j + 1] {
-                if j >= max_j_double {
-                    max_j_double = j;
-                    max_c_double = c;
+                if let Some(max_j) = max_j_single {
+                    if j >= max_j {
+                        max_j_double = Some(j);
+                        max_c_double = Some(c);
+                    }
+                } else {
+                    max_j_double = Some(j);
+                    max_c_double = Some(c);
                 }
             } else {
                 break;
@@ -38,12 +48,27 @@ pub fn longest_palindrome(s: String) -> String {
             j += 1;
         }
     }
-    if 1 + 2 * max_j_single > 2 * max_j_double + 2 {
-        let (c, j) = (max_c_single, max_j_single);
-        str::from_utf8(&s[c - j..=c + j]).unwrap().to_string()
-    } else {
-        let (c, j) = (max_c_double, max_j_double);
-        str::from_utf8(&s[c - j..=c + j + 1]).unwrap().to_string()
+    match (max_j_single, max_j_double) {
+        (Some(i), Some(j)) => {
+            if 1 + 2 * i > 2 + 2 * j {
+                let c = max_c_single.unwrap();
+                let j = i;
+                str::from_utf8(&s[c - j..=c + j]).unwrap().to_string()
+            } else {
+                let c = max_c_double.unwrap();
+                str::from_utf8(&s[c - j..=c + j + 1]).unwrap().to_string()
+            }
+        }
+        (Some(i), None) => {
+            let c = max_c_single.unwrap();
+            let j = i;
+            str::from_utf8(&s[c - j..=c + j]).unwrap().to_string()
+        }
+        (None, Some(j)) => {
+            let c = max_c_double.unwrap();
+            str::from_utf8(&s[c - j..=c + j + 1]).unwrap().to_string()
+        }
+        (None, None) => unreachable!(),
     }
 }
 
@@ -72,6 +97,18 @@ mod tests {
     fn test_4() {
         let s = String::from("abcdefgaaaa");
         let output = String::from("aaaa");
+        assert_eq!(output, longest_palindrome(s));
+    }
+    #[test]
+    fn test_5() {
+        let s = String::from("a");
+        let output = String::from("a");
+        assert_eq!(output, longest_palindrome(s));
+    }
+    #[test]
+    fn test_6() {
+        let s = String::from("ac");
+        let output = String::from("c");
         assert_eq!(output, longest_palindrome(s));
     }
 }
